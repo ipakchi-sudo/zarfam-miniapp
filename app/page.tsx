bcd2e1c92fb83a885a464618ad072df956579580
ipakchi-sudo <ipakchi@live.com>
2026-04-22 02:49:34 +0300

    Rename Page component to Home and simplify content

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,3 @@
-export default function Page() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>زرفام</h1>
-      <p>سیستم ثبت سفارش</p>
-
-      <a href="https://tally.so/r/NpBjQO">
-        ثبت سفارش
-      </a>
-    </main>
-  );
+export default function Home() {
+  return <div>OK</div>;
 }
